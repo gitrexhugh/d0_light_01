493a944a65b12b16a6a84b8e6c5dd168dc475b21
gitrexhugh <windhugh@gmail.com>
2018-09-17 12:46:16 +0800

    modify screen light_layout, fix seekbar function. works correct on phone

--- app/src/main/java/com/example/rex_h/d0_light_01/main_light_01.java
+++ app/src/main/java/com/example/rex_h/d0_light_01/main_light_01.java
@@ -1,5 +1,6 @@
 package com.example.rex_h.d0_light_01;
 
+import android.animation.ObjectAnimator;
 import android.app.Activity;
 import android.content.Context;
 import android.hardware.camera2.CameraManager;
@@ -7,6 +8,8 @@ import android.hardware.camera2.CameraAccessException;
 import android.os.Bundle;
 import android.text.Layout;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.RelativeLayout;
 import android.widget.SeekBar;
 import android.widget.ImageButton;
@@ -18,13 +21,10 @@ import android.graphics.Color;
 public class main_light_01 extends Activity {
 
     private int light_state;// 1: On_back_light_constant, 2: On_back_light_flash, 3: On_screen_light, 4: Off_light
-        //final RelativeLayout screen_bg= (RelativeLayout)findViewById(R.id.screen_background);
     //以image view宣告圖形按鈕
     private ImageView ibtn_screen_light;
     private ImageView ibtn_back_light;
     private ImageView ibtn_power;
-
-
     private TextView show_status;
     private String str_status;
     SeekBar sk_R;
@@ -35,12 +35,13 @@ public class main_light_01 extends Activity {
     int cG=255;
     int cB=255;
     int sk_id;
+    private ImageView img_glow;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         load_default_layout();
-
 
     }
     private void load_default_layout(){
@@ -55,7 +56,8 @@ public class main_light_01 extends Activity {
         ibtn_screen_light.setOnClickListener(ibtn_screen_light_Click);// Screen Light
         ibtn_screen_light.setImageResource(R.mipmap.screenlight0_xxxhdpi);
         ibtn_power.setOnClickListener(ibtn_power_click);// Power
-
+        img_glow=(ImageView)findViewById(R.id.glow);
+        //img_glow.setAlpha(0.0);
 
     }
 
@@ -63,11 +65,11 @@ public class main_light_01 extends Activity {
     private void lightOn(){
         show_status=(TextView)findViewById(R.id.show_status);
         str_status="light_On";
-        show_status.setText(str_status);
+        show_status.setText(str_status+"|status:"+light_state);
         ibtn_power.setImageResource(R.mipmap.light_xxxhdpi);
+        anim_control(light_state);
 
         //以下為Camera Manager相關，不適用模擬器
-
         CameraManager mCamera = (CameraManager)getSystemService(Context.CAMERA_SERVICE);
         try{
             String cameraID=mCamera.getCameraIdList()[0];
@@ -78,10 +80,11 @@ public class main_light_01 extends Activity {
     }
     private void lightOff(){
         str_status="light_Off";
-        show_status.setText(str_status);
-        ibtn_power.setImageResource(R.mipmap.light_xxxhdpi_0);
-        //以下為Camera Manager相關，不適用模擬器
 
+        ibtn_power.setImageResource(R.mipmap.light_xxxhdpi_0);
+        anim_control(light_state);
+
+        //以下為Camera Manager相關，不適用模擬器
         CameraManager mCamera = (CameraManager)getSystemService(Context.CAMERA_SERVICE);
         try{
             String cameraID=mCamera.getCameraIdList()[0];
@@ -89,6 +92,33 @@ public class main_light_01 extends Activity {
         }catch (CameraAccessException e){
             e.printStackTrace();
         }
+    }
+
+    private void anim_control(int light_state){
+        ObjectAnimator glow_alpha=ObjectAnimator.ofFloat(img_glow,"alpha", 1,0);
+        glow_alpha.setDuration(1800);
+        glow_alpha.setRepeatCount(Animation.INFINITE);
+        glow_alpha.setInterpolator(new DecelerateInterpolator());
+        ObjectAnimator glow_sacleX=ObjectAnimator.ofFloat(img_glow,"scaleX", 0,2);
+        glow_sacleX.setDuration(1800);
+        glow_sacleX.setRepeatCount(Animation.INFINITE);
+        glow_sacleX.setInterpolator(new DecelerateInterpolator());
+        ObjectAnimator glow_sacleY=ObjectAnimator.ofFloat(img_glow,"scaleY", 0,2);
+        glow_sacleY.setDuration(1800);
+        glow_sacleY.setRepeatCount(Animation.INFINITE);
+        glow_sacleY.setInterpolator(new DecelerateInterpolator());
+
+        if (light_state==1){
+            glow_alpha.start();
+            glow_sacleX.start();
+            glow_sacleY.start();
+        } else {
+            glow_alpha.end();
+            show_status.setText(str_status+"|status:"+light_state);
+
+        }
+
+
     }
 
     private void load_screen_light_layout(){
@@ -100,17 +130,10 @@ public class main_light_01 extends Activity {
         ibtn_back_light.setImageResource(R.mipmap.backlight0_xxxhdpi);
         ibtn_screen_light.setOnClickListener(ibtn_screen_light_Click);// Screen Light
         ibtn_screen_light.setImageResource(R.mipmap.screenlight1_xxxhdpi);
-
-
         light_state=3;
-        //light_behavior(light_state);
-
-
         sk_R=(SeekBar)findViewById(R.id.seekR);
         sk_G=(SeekBar)findViewById(R.id.seekG);
         sk_B=(SeekBar)findViewById(R.id.seekB);
-
-
 
         show_text=(TextView)findViewById(R.id.show_color);
         //screen_bg.setBackgroundColor(Color.argb(255,cR,cG,cB));
@@ -120,6 +143,7 @@ public class main_light_01 extends Activity {
         sk_R.setOnSeekBarChangeListener(seekbartracking);
         sk_G.setOnSeekBarChangeListener(seekbartracking);
         sk_B.setOnSeekBarChangeListener(seekbartracking);
+        lightOff();
 
 
     }
@@ -138,7 +162,6 @@ public class main_light_01 extends Activity {
     private View.OnClickListener ibtn_back_light_click=new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            //light_state = 1;
             load_default_layout();
 
         }
@@ -157,7 +180,6 @@ public class main_light_01 extends Activity {
         }
     };
     private SeekBar.OnSeekBarChangeListener seekbartracking=new SeekBar.OnSeekBarChangeListener(){
-        //cR=sk_R.getProgress();
 
         @Override
         public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
@@ -177,10 +199,8 @@ public class main_light_01 extends Activity {
                     break;
 
             }
-            //cR=sk_R.getProgress();
-            //screen_bg.setBackgroundColor(Color.argb(255,cR,cG,cB));
             show_text.setBackgroundColor(Color.argb(255,cR,cG,cB));
-            show_text.setText("status:"+light_state+";"+String.format("%02x", cR)+String.format("%02x", cG)+String.format("%02x", cB));
+            //show_text.setText("status:"+light_state+";"+String.format("%02x", cR)+String.format("%02x", cG)+String.format("%02x", cB));
 
         }
 
